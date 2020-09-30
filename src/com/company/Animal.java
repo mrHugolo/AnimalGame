71a2e955772f45d23c294afce4e74a181e3d743c
mrHugolo <hugo.granath@hotmail.com>
2020-09-30 10:29:55 +0200

    Animal. Added MAX_AGE, age, MAX_PRICE and price to constructor. Added all getters

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -6,11 +6,44 @@ public abstract class Animal {
     //0 = male, 1 = female
     private int gender;
     private int health;
+    private final double MAX_AGE = 30;
+    private int age;
+    private final double MAX_PRICE = 50000;
+    private double price;
 
-    public Animal(String name, int gender){
+    public Animal(String name, int gender) {
         this.name = name;
         this.gender = gender;
         health = 100;
+        age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenderString() {
+        return getGender() == 0 ? "male" : "female";
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getPrice() {
+        //price increase linearly the first 1/3 of its maximum lifespan
+        //but price decrease linearly the last 2/3 of its maximum lifespan
+        price = age <= MAX_AGE / 3 ? MAX_PRICE * ((3 * age) / (5 * MAX_AGE) + 0.8)
+                : MAX_PRICE * (1.5 - (3 * age) / (2 * MAX_AGE));
+        return (int) price;
     }
 
 }
